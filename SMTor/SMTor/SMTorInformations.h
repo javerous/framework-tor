cb7bf359cc2a4265a499f7f1d2c2786da5c6139b
Julien-Pierre Avérous <git@sourcemac.com>
2016-08-11 02:12:59 +0200

    - Fix header.

--- SMTor/SMTor/SMTorInformations.h
+++ SMTor/SMTor/SMTorInformations.h
@@ -1,13 +1,27 @@
-//
-//  SMTorInformations.h
-//  SMTor
-//
-//  Created by Julien-Pierre Avérous on 10/08/2016.
-//  Copyright © 2016 Julien-Pierre Avérous. All rights reserved.
-//
+/*
+ *  SMTorInformations.h
+ *
+ *  Copyright 2016 Avérous Julien-Pierre
+ *
+ *  This file is part of SMTor.
+ *
+ *  SMTor is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SMTor is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SMTor.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-#ifndef SMTorInformations_h
-# define SMTorInformations_h
+
+#pragma once
 
 
 /*
@@ -121,5 +135,3 @@ typedef enum
 	
 	SMTorErrorInternal
 } SMTorErrorOperation;
-
-#endif /* SMTorInformations_h */
